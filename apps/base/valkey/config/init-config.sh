b378e7d5a185d20680b0ce87df746b07e1dd6ae9
Wyller Gomes <wyller.gomes@plantsuite.com>
2026-01-10 02:08:48 -0300

    Correção na validação de quantidade de réplicas no statefulset do Valkey e ajuste para rodar em single réplica.

--- apps/base/valkey/config/init-config.sh
+++ apps/base/valkey/config/init-config.sh
@@ -4,7 +4,12 @@
 echo "Configuring for ${REPLICAS} replicas"
 
 cp /etc/valkey/valkey.conf /tmp/valkey.conf
-if [ "${REPLICAS}" -gt "1" ]; then
+if [ "${REPLICAS}" -eq "1" ]; then
+  sed -i 's/cluster-enabled yes/cluster-enabled no/' /tmp/valkey.conf
+  echo requirepass "${VALKEY_PASSWORD}" >> /tmp/valkey.conf
+else
+  # Ensure cluster is enabled for multi-node
+  sed -i 's/cluster-enabled no/cluster-enabled yes/' /tmp/valkey.conf
   echo "replica-announce-ip ${POD_IP}" >> /tmp/valkey.conf
   echo "cluster-announce-ip ${POD_IP}" >> /tmp/valkey.conf
   echo requirepass "${VALKEY_PASSWORD}" >> /tmp/valkey.conf
